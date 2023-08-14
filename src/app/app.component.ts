9891f7ec37e4eabf194f82c3d33dbf910bc5cb06
Mauricio Bayas <X018328@avidxchange.com>
2023-08-14 17:05:29 -0400

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Pipes';
+
+  name: string = "";
+  date: string = "";
+  amount: string = "";
+  miles: string = "";
+
+  onNameChange = (event: Event) => {
+    this.name = (event.target as HTMLInputElement).value;
+  }
+
+  onDateChange = (event: Event) => {
+    this.date = (event.target as HTMLInputElement).value;
+  }
+
+  onAmountChange = (event: Event) => {
+    this.amount = (event.target as HTMLInputElement).value;
+    parseFloat(this.amount).toFixed(2);
+  }
+
+  onMilesChange = (event: Event) => {
+    this.miles = (event.target as HTMLInputElement).value;
+    parseFloat(this.miles).toFixed(2);
+   }
 }
